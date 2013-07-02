d9b1412d2c4a2b5224839ed39a14ff9e6a038184
Nico Verbruggen <nico.verb@gmail.com>
2013-07-02 12:05:26 +0200

    Added a test file which grabs repo data from the API and displays it on a simple page

--- scripts/mylibs/main.js
+++ scripts/mylibs/main.js
@@ -8,5 +8,28 @@
 /* Do things on DOM load */
 
 $(function(){
-
+	getSomeData();
 });
+
+/**
+ * getSomeData() will try and fetch data from the repo
+ * @returns {undefined}
+ */
+function getSomeData(){
+	$.ajax({
+        type: "GET",
+        dataType: "json",
+        contentType: "application/json",
+        cache: false,
+        url: "https://api.github.com/repos/oSoc13/oSoctocat/stats/code_frequency",
+        beforeSend: function(xhr) {
+        },
+        success: function(data){
+			var phptimestamp = data[0][0];
+			var date = new Date(phptimestamp*1000);
+			$("body").append("<p>Timestamp of date: " + date + "</p>");
+			$("body").append("<p>Additions: " + data[0][1] + "</p>");
+			$("body").append("<p>Deletions: " + data[0][2] + "</p>");
+		}
+		});
+}
