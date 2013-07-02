a7c2b5766f1e36377f4b55db60b3b6f04ae665d2
StefChri <stefaan.ch@gmail.com>
2013-07-02 15:21:12 +0200

    api dunno whut

--- api/api.php
+++ api/api.php
@@ -5,25 +5,26 @@ require_once '../vendor/autoload.php';
 
 header('Access-Control-Allow-Origin: *');
 
+slim\Slim\Slim::registerAutoloader();
 // Create an instance of Slim
-$app = new \Slim\Slim();
+$app = new slim\Slim\Slim();
 
-$app->get('/api/:path', function ($path) {
-    $client = new Github\Client();
-
-    $client = new Github\Client();
-    $method = Github\Client::AUTH_URL_CLIENT_ID;
-    $filename = "http://localhost:8888/oSoctocat/config/githubApp.txt";
-    $lines = file($filename, FILE_IGNORE_NEW_LINES);
-    $usernameOrToken = $lines[1];
-    $password = $lines[3];
-    $client->authenticate($usernameOrToken, $password, $method);
-
-    $contributors = $client->getHttpClient()->get($path);
-    echo $contributors->getContent();
+$app->get('/api/:name', function ($name) {
+//    $client = new Github\Client();
+//    $method = Github\Client::AUTH_URL_CLIENT_ID;
+//    $filename = "http://localhost:8888/oSoctocat/config/githubApp.txt";
+//    $lines = file($filename, FILE_IGNORE_NEW_LINES);
+//    $usernameOrToken = $lines[1];
+//    $password = $lines[3];
+//    $client->authenticate($usernameOrToken, $password, $method);
+//
+//    $contributors = $client->getHttpClient()->get($path);
+//    echo $contributors->getContent();
+    //header('Content-Type', 'application/json');
+            echo $name;
 });
 
 // Run Slim app
-$app->response()->header('Content-Type', 'text/json');
+
 $app->run();
 
